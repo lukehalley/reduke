fc3e26507bb38377ba01288906420e9b30b9a775
lukehalley <luke123halley@gmail.com>
2019-02-13 12:30:33 +0000

    added register logic and sending id to shared preferences

--- Reduke/src/main/java/org/wit/reduke/activities/RedukeRegisterActivity.kt
+++ Reduke/src/main/java/org/wit/reduke/activities/RedukeRegisterActivity.kt
@@ -2,18 +2,19 @@ package org.wit.reduke.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.post.R
 import org.wit.reduke.main.MainApp
-import org.wit.reduke.models.UserModel
 
-class RedukeRegisterActivity : AppCompatActivity(), AnkoLogger {
+class HillfortRegisterActivity : AppCompatActivity(), AnkoLogger {
 
-    var user = UserModel()
     lateinit var app: MainApp
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,33 +22,47 @@ class RedukeRegisterActivity : AppCompatActivity(), AnkoLogger {
         toolbarRegister.title = title
         setSupportActionBar(toolbarRegister)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        info("Reduke Activity started..")
 
         app = application as MainApp
-
         registerButton.setOnClickListener {
-            var users = app.users.findAll()
-            if (enteredEmail.text.toString() !in users.toString()) {
-                user.name = enteredName.text.toString()
-                user.email = enteredEmail.text.toString()
-                user.password = enteredPassword.text.toString()
-                if (user.name.isEmpty() or user.email.isEmpty() or user.password.isEmpty()) {
-                    toast(R.string.hint_EnterPostTitle)
-                } else {
-                    if (enteredPassword.text.toString() == enteredPasswordConfirm.text.toString()) {
-                        app.users.create(user.copy())
-                        setResult(AppCompatActivity.RESULT_OK)
-                        toast(R.string.hint_SucessfullRegister)
-                        finish()
-                    } else {
-                        toast("Passwords Do Not Match!")
+            // Show loading indicator after the user clicks the register button.
+            showProgress()
+            auth.createUserWithEmailAndPassword(enteredEmail.text.toString(), enteredPassword.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (enteredName.text.toString().isEmpty() or enteredPassword.text.toString().isEmpty()) {
+                            toast(R.string.hint_EnterAllFields)
+                        } else {
+                            if (enteredPassword.text.toString() == enteredPasswordConfirm.text.toString()) {
+                                if (task.isSuccessful) {
+                                    val mypreference = RedukeSharedPreferences(this)
+                                    mypreference.setCurrentUserID(auth.uid.toString())
+                                    // Sign in success, update UI with the signed-in user's information
+                                    toast(R.string.hint_SucessfullRegister)
+                                    val user = auth.currentUser
+                                    finish()
+                                } else {
+                                    toast("User Registration Failed!" + task.exception)
+                                    info { "User Registration Failed!" + task.exception }
+                                }
+                            } else {
+                                toast("Passwords Do Not Match!")
+                            }
+                        }
+
+                        // Hide loading indicator after the user registers.
+                        hideProgress()
                     }
-                }
-            } else {
-                toast(R.string.err_UserExists)
-            }
+
         }
 
+    }
+
+    fun showProgress() {
+        loadingRegisterIndicator.visibility = View.VISIBLE
+    }
+
+    fun hideProgress() {
+        loadingRegisterIndicator.visibility = View.GONE
     }
 
 }
