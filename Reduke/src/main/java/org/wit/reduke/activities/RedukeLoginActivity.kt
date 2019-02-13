9ebf202eaf1bad7ad9ad4a735be75b994c5e345f
lukehalley <luke123halley@gmail.com>
2019-02-13 14:31:25 +0000

    login working

--- Reduke/src/main/java/org/wit/reduke/activities/RedukeLoginActivity.kt
+++ Reduke/src/main/java/org/wit/reduke/activities/RedukeLoginActivity.kt
@@ -2,39 +2,67 @@ package org.wit.reduke.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.google.firebase.FirebaseApp
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
-import org.jetbrains.anko.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivityForResult
+import org.jetbrains.anko.toast
 import org.wit.post.R
 import org.wit.reduke.main.MainApp
-import org.wit.reduke.models.UserModel
 
 class RedukeLoginActivity : AppCompatActivity(), AnkoLogger {
+
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     lateinit var app: MainApp
     override fun onCreate(savedInstanceState: Bundle?) {
-        FirebaseApp.initializeApp(this)
         app = application as MainApp
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val mypreference = RedukeSharedPreferences(this)
+        val redukeSharedPref = RedukeSharedPreferences(this)
+
         loginButton.setOnClickListener {
-            var users = app.users.findAll()
-            var foundUser: UserModel? = users.find { p -> p.email == enteredEmail.text.toString() }
-            if (foundUser != null) {
-                if (enteredEmail.text.toString() == foundUser.email && enteredPassword.text.toString() == foundUser.password) {
-                    mypreference.setCurrentUserName(foundUser.name)
-                    mypreference.setCurrentUserEmail(foundUser.email)
-                    mypreference.setCurrentUserPassword(foundUser.password)
-                    startActivityForResult(intentFor<RedukeListActivity>().putExtra("loggedInUser", enteredEmail.text.toString()), 0)
-                } else {
-                    toast(R.string.toast_InvalidCreds)
-                }
+            if (enteredEmail.text.toString().isNotEmpty() && enteredPassword.text.toString().isNotEmpty()) {
+                showProgress()
+                auth.signInWithEmailAndPassword(enteredEmail.text.toString(), enteredPassword.text.toString())
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                redukeSharedPref.setCurrentUserName(enteredEmail.text.toString())
+                                redukeSharedPref.setCurrentUserEmail(enteredEmail.text.toString())
+                                redukeSharedPref.setCurrentUserPassword(enteredPassword.text.toString())
+                                startActivityForResult(intentFor<RedukeListActivity>().putExtra("loggedInUser", enteredEmail.text.toString()), 0)
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                toast(R.string.toast_InvalidCreds)
+                            }
+                            if (!task.isSuccessful) {
+                                toast(R.string.toast_AuthFail)
+                            }
+                            hideProgress()
+                        }
+            } else if (enteredEmail.text.toString().isEmpty()) {
+                toast("Please Enter Your Email")
+            } else if (enteredPassword.text.toString().isEmpty()) {
+                toast("Please Enter Your Email")
             } else {
-                error { "foundUser is null" }
+                toast(R.string.hint_EnterAllFields)
             }
         }
+
         navToRegisterButton.setOnClickListener {
-            startActivityForResult<RedukeRegisterActivity>(0)
+            startActivityForResult<RedukeListActivity>(0)
         }
     }
+
+    fun showProgress() {
+        loadingLoginIndicator.visibility = View.VISIBLE
+    }
+
+    fun hideProgress() {
+        loadingLoginIndicator.visibility = View.GONE
+    }
+
 }
