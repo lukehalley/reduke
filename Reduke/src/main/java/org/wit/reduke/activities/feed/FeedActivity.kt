3b2077f3e173b1e90f28a2a0639472e0b0bc5b77
lukehalley <luke123halley@gmail.com>
2019-03-21 20:33:23 +0000

    subreddits now in sidebar

--- Reduke/src/main/java/org/wit/reduke/activities/feed/FeedActivity.kt
+++ Reduke/src/main/java/org/wit/reduke/activities/feed/FeedActivity.kt
@@ -128,62 +128,11 @@ class FeedActivity : AppCompatActivity(), RedukeListener, AnkoLogger {
             true
         }
 
-//        // Add Subreddits To Nav Drawer
-//        val menu = navigationView.menu
-//        var subs = resources.getStringArray(R.array.subreddits_list)
-//        for (i in subs) {
-//            val id = "nav_" + i.replace("\\s+", " ")
-//
-//            menu.add(i)
-//
-//            // Set Id
-//        }
-
-//        val data: HashMap<String, List<String>>
-//            get() {
-//
-//
-//                return listData
-//            }
-
         val listData = HashMap<String, List<String>>()
 
-        val redmiMobiles = ArrayList<String>()
-        redmiMobiles.add("Redmi Y2")
-        redmiMobiles.add("Redmi S2")
-        redmiMobiles.add("Redmi Note 5 Pro")
-        redmiMobiles.add("Redmi Note 5")
-        redmiMobiles.add("Redmi 5 Plus")
-        redmiMobiles.add("Redmi Y1")
-        redmiMobiles.add("Redmi 3S Plus")
+        val subs = ArrayList<String>(Arrays.asList(*resources.getStringArray(org.wit.reduke.R.array.subreddits_list)))
 
-        val micromaxMobiles = ArrayList<String>()
-        micromaxMobiles.add("Micromax Bharat Go")
-        micromaxMobiles.add("Micromax Bharat 5 Pro")
-        micromaxMobiles.add("Micromax Bharat 5")
-        micromaxMobiles.add("Micromax Canvas 1")
-        micromaxMobiles.add("Micromax Dual 5")
-
-        val appleMobiles = ArrayList<String>()
-        appleMobiles.add("iPhone 8")
-        appleMobiles.add("iPhone 8 Plus")
-        appleMobiles.add("iPhone X")
-        appleMobiles.add("iPhone 7 Plus")
-        appleMobiles.add("iPhone 7")
-        appleMobiles.add("iPhone 6 Plus")
-
-        val samsungMobiles = ArrayList<String>()
-        samsungMobiles.add("Samsung Galaxy S9+")
-        samsungMobiles.add("Samsung Galaxy Note 7")
-        samsungMobiles.add("Samsung Galaxy Note 5 Dual")
-        samsungMobiles.add("Samsung Galaxy S8")
-        samsungMobiles.add("Samsung Galaxy A8")
-        samsungMobiles.add("Samsung Galaxy Note 4")
-
-        listData["Redmi"] = redmiMobiles
-        listData["Micromax"] = micromaxMobiles
-        listData["Apple"] = appleMobiles
-        listData["Samsung"] = samsungMobiles
+        listData["Subreddits"] = subs
 
         info { "Expand List: " + listData }
 
@@ -198,13 +147,20 @@ class FeedActivity : AppCompatActivity(), RedukeListener, AnkoLogger {
             adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
             expandableListView!!.setAdapter(adapter)
 
-            expandableListView!!.setOnGroupExpandListener { groupPosition -> Toast.makeText(applicationContext, (titleList as ArrayList<String>)[groupPosition] + " List Expanded.", Toast.LENGTH_SHORT).show() }
+            expandableListView!!.setOnGroupExpandListener {
+                //                groupPosition -> Toast.makeText(applicationContext, (titleList as ArrayList<String>)[groupPosition] + " List Expanded.", Toast.LENGTH_SHORT).show()
+            }
 
-            expandableListView!!.setOnGroupCollapseListener { groupPosition -> Toast.makeText(applicationContext, (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.", Toast.LENGTH_SHORT).show() }
+            expandableListView!!.setOnGroupCollapseListener {
+            }
 
             expandableListView!!.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
                 Toast.makeText(applicationContext, "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(titleList as ArrayList<String>)[groupPosition]]!!.get(childPosition), Toast.LENGTH_SHORT).show()
                 false
+
+//                startActivityForResult<PostAddEditActivity>(0) false
+
+
             }
         }
 
