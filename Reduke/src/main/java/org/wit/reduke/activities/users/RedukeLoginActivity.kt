e56abe67cda9dc2e272ac39f49a762e389c00a41
lukehalley <luke123halley@gmail.com>
2019-03-29 15:59:04 +0000

    google sign in working

--- Reduke/src/main/java/org/wit/reduke/activities/users/RedukeLoginActivity.kt
+++ Reduke/src/main/java/org/wit/reduke/activities/users/RedukeLoginActivity.kt
@@ -1,8 +1,14 @@
 package org.wit.reduke.activities.users
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.AnkoLogger
@@ -14,12 +20,23 @@ import org.wit.reduke.main.MainApp
 import org.wit.reduke.models.posts.PostsFirebaseStore
 import org.wit.reduke.tools.EspressoIdlingResource
 
+
 class RedukeLoginActivity : AppCompatActivity(), AnkoLogger {
 
-    // Get instance of the the Firebase Auth
+    // Get instance of the Firebase Auth
     var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
-    // Get instance of the the Firebase Database
+    // Get instance of the Google Sign In
+    val RC_SIGN_IN = 1
+
+    // Configure sign-in to request the user's ID, email address, and basic
+    // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+    var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+
+    // Get instance of the Firebase Database
     var fireStore: PostsFirebaseStore? = null
 
     lateinit var app: MainApp
@@ -35,7 +52,7 @@ class RedukeLoginActivity : AppCompatActivity(), AnkoLogger {
         val redukeSharedPref = RedukeSharedPreferences(this)
 
         // Setup the login button functionality.
-        loginButton.setOnClickListener {
+        firebaseLoginButton.setOnClickListener {
             // Make sure the fields are not empty, if they are tell the user to fill them.
             if (enteredLoginEmail.text.toString().isNotEmpty() && enteredLoginPassword.text.toString().isNotEmpty()) {
                 // Tell Espresso to wait for UI testing as we do not know how long the Firebase login will take.
@@ -84,6 +101,15 @@ class RedukeLoginActivity : AppCompatActivity(), AnkoLogger {
         navToRegisterButton.setOnClickListener {
             startActivityForResult<RedukeRegisterActivity>(0)
         }
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        googleLoginButton.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
     }
 
     // Functions to show and hide loading animation.
@@ -93,6 +119,44 @@ class RedukeLoginActivity : AppCompatActivity(), AnkoLogger {
 
     fun hideProgress() {
         loadingLoginIndicator.visibility = View.GONE
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        val redukeSharedPref = RedukeSharedPreferences(this)
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            if (account != null) {
+                val personName = account.displayName
+                val personGivenName = account.givenName
+                val personFamilyName = account.familyName
+                val personEmail = account.email
+                val personId = account.id
+                val personPhoto = account.photoUrl
+
+                if (personEmail != null) {
+                    redukeSharedPref.setCurrentUserEmail(personEmail)
+                }
+
+                // Signed in successfully, show authenticated UI.
+                startActivityForResult(intentFor<FeedActivity>().putExtra("loggedInUser", personEmail), 0)
+            }
+        } catch (e: ApiException) {
+            toast(org.wit.reduke.R.string.toast_GoogleAuthFail)
+        }
+
     }
 
 }
